5e5ce8bcd43769ea8fba1f8bb086104d8e8b09c7
fghgrtugd <fghgrtugd@gmail.com>
2025-07-17 10:29:18 +0300

    1.8. Класс использующий сам себя [#167388 #548489]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -13,7 +13,7 @@ public class Battery {
 
     public void exchange(Battery another) {
         another.load += load;
-        this.load -= load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
